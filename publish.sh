9563ebb6fe6240c3b493b394335a0b2440f6e294
Marvin Lee Fimmel <kontakt@ekranos.me>
2023-12-20 15:23:31 +0100

    Bumped version to 0.0.9 and added github actions

---
+++ publish.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+VERSION=$(cat mod/info.json | jq -r '.version')
+MODID=$(cat mod/info.json | jq -r '.name')
+DIRNAME="$MODID\_$VERSION"
+ZIPPATH="$DIRNAME.zip"
+
+cp -r "mod" "$DIRNAME"
+
+zip -r "$ZIPPATH" "$DIRNAME"
+
+factorio-api publish --api-key "$FACTORIO_API_KEY" \
+                     --mod "$MODID" \
+                     --file "$ZIPPATH"
